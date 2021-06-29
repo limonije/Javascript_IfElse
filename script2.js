2280211bdbef6b7660ecff4f5486550eaaa454a5
limonije <mroozen@live.nl>
2021-06-29 17:22:56 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,35 @@
+const age = 18;
+
+if (age >= 18) {
+
+    console.log("Welkom, je mag naar binnen!");
+
+} else {
+
+    console.log("Helaas, je bent te jong. Je mag niet naar binnen!");
+
+}
+
+const isFemale = false;
+
+if (isFemale) {
+
+    console.log("Het is vanavond ladiesnight. Veel plezier!");
+
+} else {
+
+    console.log("Sorry, mannen zijn niet welkom vanavond");
+
+}
+
+const driverStatus = "alcohol";
+
+if (driverStatus == "bob") {
+
+    console.log("We hopen dat je een fijne avond hebt gehad. Rij voorzichtig!");
+
+} else {
+
+    console.log("Helaas, je hebt teveel gedronken. Bel een taxi!");
+
+}
